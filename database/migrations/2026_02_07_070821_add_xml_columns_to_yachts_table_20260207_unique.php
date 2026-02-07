a7081d54f0d587735199faf80efef08f480fbb7a
GitHub Copilot <git@github.com>
2026-02-07 08:12:01 +0100

    Shinra

--- database/migrations/2026_02_07_070821_add_xml_columns_to_yachts_table_20260207_unique.php
+++ database/migrations/2026_02_07_070821_add_xml_columns_to_yachts_table_20260207_unique.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAllMissingYachtColumnsSafely extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -112,4 +112,4 @@ class AddAllMissingYachtColumnsSafely extends Migration
             }
         });
     }
-}
+};
