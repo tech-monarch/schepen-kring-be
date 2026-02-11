410fe30632a15ffce822ac83b2b1d2fd1f7bedb3
GitHub Copilot <git@github.com>
2026-02-11 17:22:43 +0100

    yes

--- database/migrations/2026_02_11_153903_add_missing_yacht_columns.php
+++ database/migrations/2026_02_11_153903_add_missing_yacht_columns.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class BackfillYachtBoatName extends Migration
+class BackfillYachtBoatNameData extends Migration
 {
     public function up()
     {
