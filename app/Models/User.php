0cd148d7352cc79b2fee6be4c4a6cb30d8092c11
GitHub Copilot <git@github.com>
2026-02-14 04:26:53 +0100

    Shinra Tensei

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
